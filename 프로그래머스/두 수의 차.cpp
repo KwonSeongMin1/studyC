16ce80f00365faeb8fab8c1113cbb162ceac0819
KwonSeongMin1 <smin9011@gmail.com>
2023-04-05 18:58:37 +0900

    window commit

---
+++ 프로그래머스/두 수의 차.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = num1 - num2;
+    return answer;
+}
+
+int main() {
+
+    cout << solution(4, 3) << endl;
+
+    return 0;
+}
